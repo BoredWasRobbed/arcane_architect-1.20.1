d23bb017ed98f46d7ef6afa239dbba2593b05afb
BoredWasRobbed <bored7160@gmail.com>
2025-12-06 23:20:13 -0500

    Basic Setup

---
+++ src/main/java/net/bored/ArcaneArchitectClient.java
@@ -0,0 +1,11 @@
+package net.bored;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ArcaneArchitectClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
